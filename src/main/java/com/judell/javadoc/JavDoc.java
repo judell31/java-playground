329b2384a8c71fdadacc23f0a78b0439539858a7
Justin Davis <justintdavis8@gmail.com>
2019-09-14 00:42:57 -0500

    Create JavDoc.java

---
+++ src/main/java/com/judell/javadoc/JavDoc.java
@@ -0,0 +1,24 @@
+package com.judell.javadoc;
+
+/**
+ * I am a java doc
+ */
+public class JavDoc {
+
+    /**
+     * Java doc
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("This is a java doc");
+        hello("Justin!!!");
+    }
+
+    /**
+     * This method takes in a string param and says hello plus
+     * what ever you pass into the method
+     */
+    public static void hello(String string){
+        System.out.println("Hello " + string);
+    }
+}

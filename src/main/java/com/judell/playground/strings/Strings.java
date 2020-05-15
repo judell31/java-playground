7815a723fa8ad8a567df02ae9929a10cc1d91a0c
Justin Davis <justintdavis8@gmail.com>
2020-05-15 09:41:23 -0500

    Update Strings.java

--- src/main/java/com/judell/playground/strings/Strings.java
+++ src/main/java/com/judell/playground/strings/Strings.java
@@ -5,6 +5,8 @@ public class Strings {
     public static void main(String[] args) {
         String ch = "t";
         String test = "test";
+        String[] strArray = {"test", "me", "and", "you"};
+
         System.out.println(ch.charAt(0) == test.charAt(0));
 
         System.out.println("String:\tA\n".replace("\t", "\\t").replace("\n","\\n"));
@@ -13,5 +15,11 @@ public class Strings {
         System.out.println(x.contains("e"));
         char c = 10;
         System.out.println("d\n".charAt(1) == c);
+
+        int count = 0;
+        for (int i = 0; i < strArray.length; i++){
+            count++;
+        }
+        System.out.println(count);
     }
 }

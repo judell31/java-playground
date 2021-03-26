918e28494e30cbe53b4337f1312c1a2dcd91cd18
jtdavis <justintdavis8@gmail.com>
2021-03-26 16:53:51 -0500

    Update UTCTimeStamp.java

--- src/main/java/com/judell/playground/time_stamp/UTCTimeStamp.java
+++ src/main/java/com/judell/playground/time_stamp/UTCTimeStamp.java
@@ -1,6 +1,9 @@
 package com.judell.playground.time_stamp;
 
 import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.util.Calendar;
 import java.util.TimeZone;
 
@@ -18,5 +21,25 @@ public class UTCTimeStamp {
          */
         TimeZone timeZone = Calendar.getInstance().getTimeZone();
         System.out.println("TimeZone: " + timeZone);
+
+        /*
+        This may be wrong
+         */
+        ZoneId fromTimeZone = ZoneId.of("Etc/UTC");    //Source timezone
+        ZoneId toTimeZone = ZoneId.of(timeZone.getID());  //Target timezone
+
+        LocalDateTime today = LocalDateTime.now();          //Current time
+        ZonedDateTime currentUtcTime = today.atZone(fromTimeZone);
+        ZonedDateTime currentUsTime = currentUtcTime.withZoneSameInstant(toTimeZone);
+
+        //Zoned date time at source timezone
+        ZonedDateTime currentISTime = today.atZone(fromTimeZone);
+
+        //Zoned date time at target timezone
+        ZonedDateTime currentETime = currentISTime.withZoneSameInstant(toTimeZone);
+
+        //Format date time - optional
+        System.out.println(currentUtcTime);
+        System.out.println(currentUsTime);
     }
 }

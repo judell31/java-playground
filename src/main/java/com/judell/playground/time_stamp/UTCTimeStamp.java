7891dd31a0e2eb3401f71f9c88281efbb0ed8c82
jtdavis <justintdavis8@gmail.com>
2021-03-26 15:22:51 -0500

    Create UTCTimeStamp.java

---
+++ src/main/java/com/judell/playground/time_stamp/UTCTimeStamp.java
@@ -0,0 +1,13 @@
+package com.judell.playground.time_stamp;
+
+import java.time.Instant;
+
+public class UTCTimeStamp {
+
+    /**
+     * Gets Time in utc
+     */
+    public static void main(String[] args) {
+        System.out.println(Instant.now());
+    }
+}
